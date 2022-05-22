f2df59e663095ca03687027e3fef31f6726d723b
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-23 01:18:10 +0600

    implementing jwt with my products

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
 
 
 
-        app.post('/login', async (req, res) => {
+        app.post('/signin', async (req, res) => {
             const user = req.body;
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1d'
